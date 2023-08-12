a5c29076f52b5761bd237036a1c24240cef5642e
Thiago Teixeira <thiago.teixeira@snowflake.com>
2023-08-12 00:28:07 -0700

    Remove logs

--- src/modeParser.ts
+++ src/modeParser.ts
@@ -2,11 +2,9 @@ import { isElementOf } from "./array.ts"
 
 export function shouldIncludeSection(sectionName, modeSpec) {
   const include = modeSpec?.include
-  console.log("shouldIncludeSection", sectionName, modeSpec)
 
   if (include) {
     const includeThisSection = isElementOf(sectionName, include)
-    console.log(includeThisSection)
     return includeThisSection
   }
 
@@ -14,10 +12,8 @@ export function shouldIncludeSection(sectionName, modeSpec) {
 
   if (exclude) {
     const excludeThisSection = isElementOf(sectionName, exclude)
-    console.log(!excludeThisSection)
     return !excludeThisSection
   }
 
-  console.log(true)
   return true
 }
