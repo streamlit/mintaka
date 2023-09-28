cd3eb030714ce4c80f91b022953683e72c2cd372
Thiago Teixeira <thiago.teixeira@snowflake.com>
2023-09-27 17:53:22 -0700

    Add subpath for Github page deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,20 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
   server: {
+    // Dev server port
     port: 3456
-  }
+  },
+  // URL subpath when published to Github pages.
+  // i.e. https://streamlit.github.io/deneb/
+  base: '/deneb/',
+  test: {
+    environment: 'jsdom',
+    setupFiles: [
+      'testSetup.js',
+    ],
+  },
 })
